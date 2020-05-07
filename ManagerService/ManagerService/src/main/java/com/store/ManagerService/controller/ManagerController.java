0382e1abff7fb641adbe29106efcae68313dbb0a
batch5ba <batch5ba@gmail.com>
2020-05-07 15:41:18 +0530

    first commit

---
+++ ManagerService/ManagerService/src/main/java/com/store/ManagerService/controller/ManagerController.java
@@ -0,0 +1,59 @@
+package com.store.ManagerService.controller;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store.ManagerService.model.EmployeePojo;
+import com.store.ManagerService.service.ManagerService;
+
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("store")
+
+public class ManagerController {
+	
+	@Autowired
+	ManagerService managerService;
+	
+	@PostMapping("/employee")
+	EmployeePojo addEmployee(@RequestBody EmployeePojo empPojo)
+	{
+		return managerService.addEmployee(empPojo);
+	}
+	
+	@DeleteMapping("/employee/{empId}")
+	public  Object deleteEmployee(@PathVariable("empId") int empId)
+	{
+		return managerService.deleteEmployee(empId);
+	}
+	@PutMapping("/employee")
+	EmployeePojo updateEmployee(@RequestBody EmployeePojo empPojo) {
+		
+		return managerService.updateEmployee(empPojo);
+	}
+	@GetMapping("/employee/all")
+	List<EmployeePojo> getAllEmployees(){
+		
+		return managerService.getAllEmployees();	
+	}
+	@Test
+	@GetMapping("/employee/{empId}")
+	EmployeePojo getEmployee(@PathVariable("empId") int empId)
+	{
+		return managerService.getEmployee(empId);
+	}
+
+}
